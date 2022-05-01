fe56b9e2664fc14fdb13b6cbbabba45a7736cffa
john doe <camimosquera2@hotmail.com>
2022-05-01 11:39:53 -0500

    Ejercicio 3

--- Tarea_1.js
+++ Tarea_1.js
@@ -13,3 +13,16 @@ var suma = parseFloat(numDecimal)+parseInt(numEntero);
 
 console.log("El resultado de la suma es " + suma);
 
+//3.
+var num1 = prompt("Ingrese primer numero: ");
+var num2 = prompt("Ingrese segundo numero: ");
+
+var suma = parseInt(num1) + parseInt(num2);
+
+console.log("El resultado de la suma es: " + suma)
+
+var num3 = prompt("Ingrese tercer numero: ");
+
+var multiplicacion = suma * parseInt(num3);
+
+console.log("Resultado total: " + multiplicacion)
