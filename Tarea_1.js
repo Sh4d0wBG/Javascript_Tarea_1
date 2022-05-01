dd695c83fada047b4519fc664bcdcc67d8bbe62a
camilo0415 <camimosquera2@hotmail.com>
2022-05-01 11:38:44 -0500

    Ejercicio2

--- Tarea_1.js
+++ Tarea_1.js
@@ -5,5 +5,11 @@ var saludar = function(nombre){
 
 console.log(saludar("Patricia"));
 
+//2.
+var numDecimal = prompt("Ingrese numero decimal: ");
+var numEntero = prompt("Ingrese numero entero: ");
 
+var suma = parseFloat(numDecimal)+parseInt(numEntero);
+
+console.log("El resultado de la suma es " + suma);
 
